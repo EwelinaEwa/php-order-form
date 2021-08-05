7c0be4bb626b1ee624f49d1bc547c296ec48bc58
EwelinaEwa <ewelina.kosciukiewicz@gmail.com>
2021-08-05 14:55:33 +0200

    Move product list to a seperate file

--- index.php
+++ index.php
@@ -12,11 +12,11 @@ error_reporting(E_ALL);
 
 require "orders.php";
 require "products.php";
+require "product-list.php";
 
 
 // We are going to use session variables so we need to enable sessions
 session_start();
-
 
 
 // Use this function when you need to need an overview of these variables
@@ -34,25 +34,6 @@ function whatIsHappening() {
 }
 
 whatIsHappening();
-
-$product1 = new Product("PHP Novice - brown charm", 100);
-$product2 = new Product("PHP Apprentice - yellow charm", 500);
-$product3 = new Product("PHP Bodger - blue charm", 1000);
-$product4 = new Product("PHP Journeyman - green charm", 5000);
-$product5 = new Product("PHP Scholar - purple charm", 10000);
-$product6 = new Product("PHP Master - red charm", 20000);
-$product7 = new Product("PHP Grand Master - gold charm", 50000);
-$product8 = new Product("PHP Legend - white charm", 100000);
-$product9 = new Product("PHP God - black charm", 500000);
-$product10 = new Product("JS Novice - brown charm", 100);
-$product11 = new Product("JS Apprentice - yellow charm", 500);
-$product12 = new Product("JS Bodger - blue charm", 1000);
-$product13 = new Product("JS Journeyman - green charm", 5000);
-$product14 = new Product("JS Scholar - purple charm", 10000);
-$product15 = new Product("JS Master - red charm", 20000);
-$product16 = new Product("JS Grand Master - gold charm", 50000);
-$product17 = new Product("JS Legend - white charm", 100000);
-$product18 = new Product("JS God - black charm", 500000);
 
 
 $host = basename($_SERVER['REQUEST_URI']);
---
+++ product-list.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+ini_set('display_errors', '1');
+ini_set('display_startup_errors', '1');
+error_reporting(E_ALL);
+
+
+$product1 = new Product("PHP Novice - brown charm", 100);
+$product2 = new Product("PHP Apprentice - yellow charm", 500);
+$product3 = new Product("PHP Bodger - blue charm", 1000);
+$product4 = new Product("PHP Journeyman - green charm", 5000);
+$product5 = new Product("PHP Scholar - purple charm", 10000);
+$product6 = new Product("PHP Master - red charm", 20000);
+$product7 = new Product("PHP Grand Master - gold charm", 50000);
+$product8 = new Product("PHP Legend - white charm", 100000);
+$product9 = new Product("PHP God - black charm", 500000);
+$product10 = new Product("JS Novice - brown charm", 100);
+$product11 = new Product("JS Apprentice - yellow charm", 500);
+$product12 = new Product("JS Bodger - blue charm", 1000);
+$product13 = new Product("JS Journeyman - green charm", 5000);
+$product14 = new Product("JS Scholar - purple charm", 10000);
+$product15 = new Product("JS Master - red charm", 20000);
+$product16 = new Product("JS Grand Master - gold charm", 50000);
+$product17 = new Product("JS Legend - white charm", 100000);
+$product18 = new Product("JS God - black charm", 500000);
+
